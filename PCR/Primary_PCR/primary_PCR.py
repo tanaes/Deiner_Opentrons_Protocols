729226ae50f92f75b13f2a9fe137fb53a08a95f2
Tanaes <jonsan@gmail.com>
2022-10-27 09:14:36 +0200

    updates

--- PCR/Primary_PCR/primary_PCR.py
+++ PCR/Primary_PCR/primary_PCR.py
@@ -12,37 +12,33 @@ metadata = {
 
 # Define sample plates and their corresponding tip racks.
 sample_plates = {'Plate 1': {'pos': 1,
-                             'tip': 7},
-                 'Plate 2': {'pos': 2,
-                             'tip': 8}}
+                             'tip': 7}}
 
 # Define columns to transfer for PCR
-sample_cols = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6',
-               'A7', 'A8', 'A9', 'A10', 'A11', 'A12']
+sample_cols = ['A6', 'A7', 'A8']
 
+# sample_cols = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6',
+#                'A7', 'A8', 'A9', 'A10', 'A11', 'A12'
 
 ###### PCR Plates #######
 
 # Define PCR plate positions.
 pcr_plates = {'PCR 1': 3,
               'PCR 2': 4,
-              'PCR 3': 5,
-              'PCR 4': 6}
+              'PCR 3': 5}
 
 # Define MM location for each PCR plate.
 pcr_mm_map = {'PCR 1': 'A1',
-              'PCR 2': 'A2',
-              'PCR 3': 'A3',
-              'PCR 4': 'A4'
+              'PCR 2': 'A1',
+              'PCR 3': 'A1'
               }
 
 ###### Sample:PCR map #######
 
 # Link each sample plate to the PCR plate replicates
 sample_pcr_map = {'Plate 1': ['PCR 1',
-                              'PCR 2'],
-                  'Plate 2': ['PCR 3',
-                              'PCR 4']}
+                              'PCR 2',
+                              'PCR 3']}
 
 # Sample and PCR plate types
 sample_labware = 'biorad_96_wellplate_200ul_pcr'
@@ -60,7 +56,7 @@ def run(protocol: protocol_api.ProtocolContext):
     tiprack_reagents = protocol.load_labware('opentrons_96_filtertiprack_200ul', 9)
 
     # reagents
-    reagents = protocol.load_labware('usascientific_12_reservoir_22ml',
+    reagents = protocol.load_labware('tubeblockeppendorfstriptube_96_wellplate_250ul',
                                      11, 'reagents')
 
     # sample plates and tips
@@ -81,23 +77,21 @@ def run(protocol: protocol_api.ProtocolContext):
                                            p)
 
     # initialize pipettes
-    pipette_left = protocol.load_instrument('p20_multi_gen2',
-                                             'left')
-    pipette_right = protocol.load_instrument('p300_multi_gen2',
-                                             'right',
+    pipette_20 = protocol.load_instrument('p20_multi_gen2',
+                                             'right')
+    pipette_300 = protocol.load_instrument('p300_multi_gen2',
+                                             'left',
                                              tip_racks=[tiprack_reagents])
 
-    # # home instrument
-    protocol.home()
 
     # # dispense master mix
     for p in pcr_plates:
         print(p)
         print(pcr_obj[p])
-        pipette_right.distribute(mm_vol,
+        pipette_300.distribute(mm_vol,
                                  reagents[pcr_mm_map[p]], 
-                                 [pcr_obj[p][c] for c in sample_cols],
-                                 blow_out=False,
+                                 [pcr_obj[p][c].bottom(z=1) for c in sample_cols],
+                                 blow_out=True,
                                  blowout_location='source well',
                                  disposal_volume=10)
 
@@ -105,14 +99,14 @@ def run(protocol: protocol_api.ProtocolContext):
     for s in sample_plates:
         for c in sample_cols:
             print(sample_tips[s][c])
-            pipette_left.pick_up_tip(sample_tips[s][c])
-            pipette_left.distribute(sample_vol,
+            pipette_20.pick_up_tip(sample_tips[s][c])
+            pipette_20.distribute(sample_vol,
                                     sample_obj[s][c],
-                                    [pcr_obj[p][c].bottom() for p in sample_pcr_map[s]],
+                                    [pcr_obj[p][c].bottom(z=1) for p in sample_pcr_map[s]],
                                     new_tip='never',
                                     blow_out=False,
                                     disposal_volume=1,
                                     trash=False)
-            pipette_left.drop_tip()
+            pipette_20.drop_tip()
 
 
